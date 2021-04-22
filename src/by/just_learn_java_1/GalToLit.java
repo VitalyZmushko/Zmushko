cf715cef829b7247e174b744c7e653e1cfa8ab0a
Vitalya <zmuskovitaliy@gmail.com>
2021-04-22 20:12:01 +0300

    Hi

--- src/by/just_learn_java_1/GalToLit.java
+++ src/by/just_learn_java_1/GalToLit.java
@@ -7,9 +7,17 @@ public class GalToLit {
     }
 
     private static void gal_to_lit() {
-        double gallons = 10;
         double litres;
-        litres = gallons * 3.7856;
-        System.out.printf("В %.0f галлонах %.3f литров", gallons, litres);
+        int counter = 0;
+        for (double gallons = 0; gallons < 100; gallons++) {
+            litres = gallons * 3.7856;
+            System.out.printf("В %.0f галлонах %.3f литров", gallons, litres);
+            System.out.println();
+            counter++;
+            if (counter == 10) {
+                System.out.println();
+                counter = 0;
+            }
+        }
     }
 }
